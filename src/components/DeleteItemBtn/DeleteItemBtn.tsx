7ad36120cf5b9a670172703bd6378725550a45ff
Nelli Gorlova <nelli7035@gmail.com>
2024-09-28 17:32:08 +0600

    added a component - item delete button

---
+++ src/components/DeleteItemBtn/DeleteItemBtn.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './DeleteItemBtn.css';
+
+interface Props {
+    onDeleteItem: () => void;
+}
+
+const DeleteItemBtn: React.FC<Props> = ({onDeleteItem}) => {
+    return (
+        <button type='button' className='delete-item-btn' onClick={onDeleteItem}>
+            ×
+        </button>
+    );
+};
+
+export default DeleteItemBtn;
